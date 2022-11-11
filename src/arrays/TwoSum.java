dd2b261c247ba22cbf276dd07913530d9fdde89b
przemko <przemekgrabski@outlook.com>
2022-11-11 16:19:21 +0100

    Add twoSum answer

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,7 +1,23 @@
 package arrays;
 
+import java.util.HashMap;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        return new int[]{0,1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+
+            //As we shouldn't check same number twice we have to add value to map after.
+            map.put(nums[i], i);
+        }
+        return result;
     }
 }
